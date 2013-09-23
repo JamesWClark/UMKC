d05573aa9773a1db250570b24572a1f9623a7217
JamesWClark <jwclark@rockhursths.edu>
2013-09-23 03:30:30 -0500

    return stats for each sort: duration, iterations, swaps

--- CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Run.java
+++ CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Run.java
@@ -1,5 +1,7 @@
 package jwc62f.edu.umkc.assignment.two;
 
+import java.util.Calendar;
+
 public class Run {
 	
 	private static int[] numbers_nearlysorted = null;
@@ -18,29 +20,40 @@ public class Run {
 			try {
 				switch(i) {
 					case 0: //selection sort
-						System.out.println("selection sort vs nearly sorted data:  " + Sort.Selection(numbers_nearlysorted) + " milliseconds");
-						System.out.println("selection sort vs reverse sorted data: " + Sort.Selection(numbers_reverse) + " milliseconds");
-						System.out.println("selection sort vs random sorted data:  " + Sort.Selection(numbers_random) + " milliseconds");
+						System.out.println("selection sort vs nearly sorted data:  " + Sort.Selection(numbers_nearlysorted)[0] + " milliseconds");
+						System.out.println("selection sort vs reverse sorted data: " + Sort.Selection(numbers_reverse)[0] + " milliseconds");
+						System.out.println("selection sort vs random sorted data:  " + Sort.Selection(numbers_random)[0] + " milliseconds");
 						break;
 					case 1: //bubble sort
-						System.out.println("bubble sort vs nearly sorted data:     " + Sort.Bubble(numbers_nearlysorted) + " milliseconds");
-						System.out.println("bubble sort vs reverse sorted data:    " + Sort.Bubble(numbers_reverse) + " milliseconds");
-						System.out.println("bubble sort vs random sorted data:     " + Sort.Bubble(numbers_random) + " milliseconds");
+						System.out.println("bubble sort vs nearly sorted data:     " + Sort.Bubble(numbers_nearlysorted)[0] + " milliseconds");
+						System.out.println("bubble sort vs reverse sorted data:    " + Sort.Bubble(numbers_reverse)[0] + " milliseconds");
+						System.out.println("bubble sort vs random sorted data:     " + Sort.Bubble(numbers_random)[0] + " milliseconds");
 						break;
 					case 2: //insertion sort
-						System.out.println("insertion sort vs nearly sorted data:  " + Sort.Insertion(numbers_nearlysorted) + " milliseconds");
-						System.out.println("insertion sort vs reverse sorted data: " + Sort.Insertion(numbers_reverse) + " milliseconds");
-						System.out.println("insertion sort vs random sorted data:  " + Sort.Insertion(numbers_random) + " milliseconds");
+						System.out.println("insertion sort vs nearly sorted data:  " + Sort.Insertion(numbers_nearlysorted)[0] + " milliseconds");
+						System.out.println("insertion sort vs reverse sorted data: " + Sort.Insertion(numbers_reverse)[0] + " milliseconds");
+						System.out.println("insertion sort vs random sorted data:  " + Sort.Insertion(numbers_random)[0] + " milliseconds");
 						break;
 					case 3: //shell
-						System.out.println("shell sort vs nearly sorted data:      " + Sort.Shell(numbers_nearlysorted) + " milliseconds");
-						System.out.println("shell sort vs reverse sorted data:     " + Sort.Shell(numbers_reverse) + " milliseconds");
-						System.out.println("shell sort vs random sorted data:      " + Sort.Shell(numbers_random) + " milliseconds");
+						System.out.println("shell sort vs nearly sorted data:      " + Sort.Shell(numbers_nearlysorted)[0] + " milliseconds");
+						System.out.println("shell sort vs reverse sorted data:     " + Sort.Shell(numbers_reverse)[0] + " milliseconds");
+						System.out.println("shell sort vs random sorted data:      " + Sort.Shell(numbers_random)[0] + " milliseconds");
 						break;
 					case 4: //quick
-						System.out.println("quick sort vs nearly sorted data:      " + Sort.Quick(numbers_nearlysorted, 0, numbers_nearlysorted.length) + " milliseconds");
-						System.out.println("quick sort vs reverse sorted data:     " + Sort.Quick(numbers_reverse, 0, numbers_reverse.length) + " milliseconds");
-						System.out.println("quick sort vs random sorted data:      " + Sort.Quick(numbers_random, 0, numbers_random.length) + " milliseconds");
+						long start = Calendar.getInstance().getTimeInMillis();
+						int[] stats = Sort.Quick(numbers_nearlysorted, 0, numbers_nearlysorted.length);
+						long duration = Calendar.getInstance().getTimeInMillis() - start;
+						System.out.println("quick sort vs nearly sorted data:      " + duration + " milliseconds");
+						
+						start = Calendar.getInstance().getTimeInMillis();
+						stats = Sort.Quick(numbers_reverse, 0, numbers_reverse.length);
+						duration = Calendar.getInstance().getTimeInMillis() - start;
+						System.out.println("quick sort vs reverse sorted data:     " + duration + " milliseconds");
+						
+						start = Calendar.getInstance().getTimeInMillis();
+						stats = Sort.Quick(numbers_random, 0, numbers_random.length);
+						duration = Calendar.getInstance().getTimeInMillis() - start;
+						System.out.println("quick sort vs random sorted data:      " + duration + " milliseconds");
 						break;
 				}
 			} catch(Exception e) {
--- CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Sort.java
+++ CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Sort.java
@@ -4,50 +4,77 @@ import java.util.Calendar;
 
 public class Sort {
 	
-	public static long Bubble(int[] array) {
+	public static Object[] Bubble(int[] array) {
+		int iterations = 0;
+		int swaps = 0;
 		long start = Calendar.getInstance().getTimeInMillis();
 		int last = array.length;
 		for(int pass = 1; pass < last; pass++) {
+			++iterations;
 			for(int first_of_pair = 0; first_of_pair != last - pass; ++first_of_pair) {
+				++iterations;
 				int second_of_pair = first_of_pair + 1;
 				if(array[second_of_pair] < array[first_of_pair]){
 					Swap(array, second_of_pair, first_of_pair);
+					++swaps;
 				}
 			}
 		}
 		long finish = Calendar.getInstance().getTimeInMillis();
-		return (finish - start);
+		Object[] stats = new Object[3];
+		stats[0] = (finish - start);
+		stats[1] = iterations;
+		stats[2] = swaps;
+		return stats;
 	}
-	public static long Selection(int[] array) {
+	public static Object[] Selection(int[] array) {
+		int iterations = 0;
+		int swaps = 0;
 		long start = Calendar.getInstance().getTimeInMillis();
 		int size = array.length;
 		for(int fill = 0; fill != size - 1; ++fill) {
+			++iterations;
 			int pos_min = fill;
 			for(int next = fill + 1; next != size; ++next) {
+				++iterations;
 				if(array[next] < array[pos_min]) {
 					pos_min = next;
 				}
 			}
 			if (fill != pos_min) { 
 				Swap(array, fill, pos_min);
+				++swaps;
 			}
 		}
 		long finish = Calendar.getInstance().getTimeInMillis();
-		return (finish - start);
+		Object[] stats = new Object[3];
+		stats[0] = (finish - start);
+		stats[1] = iterations;
+		stats[2] = swaps;
+		return stats;
 	}
-	public static long Insertion(int[] array) {
+	public static Object[] Insertion(int[] array) {
+		int iterations = 0;
+		int swaps = 0;
 		long start = Calendar.getInstance().getTimeInMillis();
 		int first = 0;
 		int last = array.length;
 		for(int next_pos = first + 1; next_pos != last; ++next_pos) {
+			++iterations;
 			int next_val = array[next_pos];
 			while(next_pos != first && next_val < array[next_pos-1]) {
+				++iterations;
 				Swap(array, next_pos-1, next_pos--);
+				++swaps;
 			}
 			array[next_val] = next_pos;
 		}
 		long finish = Calendar.getInstance().getTimeInMillis();
-		return (finish - start);
+		Object[] stats = new Object[3];
+		stats[0] = (finish - start);
+		stats[1] = iterations;
+		stats[2] = swaps;
+		return stats;
 	}
 	//select a pivot
 	//lesser values left
@@ -56,15 +83,17 @@ public class Sort {
 		//left-side: set pivot at position 0
 		//everything right of new pivot becomes new subarray
 		//this continues until only single elements are on the right and left of a pivot - that means they are sorted
-	public static long Quick(int[] array, int from_index, int to_index) {
-		long start = Calendar.getInstance().getTimeInMillis();
+	
+	private static int quick_iterations = 0;
+	private static int quick_swaps = 0;
+	public static int[] Quick(int[] array, int from_index, int to_index) {
+		++quick_iterations;
 		if(to_index - from_index > 1) {
 			int pivot_index = Util_Partition(array, from_index, to_index);
 			Quick(array, from_index, pivot_index);
 			Quick(array, pivot_index + 1, to_index);
 		}
-		long finish = Calendar.getInstance().getTimeInMillis();
-		return (finish - start);
+		return new int[]{quick_iterations, quick_swaps};
 	}
 	//select the pivot (can be any element)
 	//find first value at left side > than pivot
@@ -74,6 +103,7 @@ public class Sort {
 		int up_index = first + 1;
 		int down_index = last - 1;
 		do {
+			++quick_iterations;
 			while((up_index != down_index) && !(array[first] < array[up_index])) {
 				++up_index;
 			}
@@ -82,27 +112,38 @@ public class Sort {
 			}
 			if(up_index < down_index) { 
 				Swap(array, up_index, down_index);
+				++quick_swaps;
 			}
 		} while (up_index < down_index);
 		
 		Swap(array, first, down_index);
+		++quick_swaps;
 		return down_index;
 	}
 	//http://stackoverflow.com/questions/4833423/shell-sort-java-example
-	public static long Shell(int[] array) {
+	public static Object[] Shell(int[] array) {
+		int iterations = 0;
+		int swaps = 0;
 		long start = Calendar.getInstance().getTimeInMillis();
         for (int gap = array.length / 2; gap > 0; gap = gap == 2 ? 1 : (int)( gap / 2.2 )) {
+        	++iterations;
             for (int i = gap; i < array.length; i++) {
+            	++iterations;
                 int temp = array[i];
                 int j = i;
                 for ( ; j >= gap && temp < array[j - gap]; j -= gap) {
                 	array[j] = array[j - gap];
                 }
                 array[j] = temp;
+                ++swaps;
             }
         }
 		long finish = Calendar.getInstance().getTimeInMillis();
-		return (finish - start);
+		Object[] stats = new Object[3];
+		stats[0] = (finish - start);
+		stats[1] = iterations;
+		stats[2] = swaps;
+		return stats;
     }
 	private static void Swap(int[] array, int a, int b) {
 		int temp = array[b];
