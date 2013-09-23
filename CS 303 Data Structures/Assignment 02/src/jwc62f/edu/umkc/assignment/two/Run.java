3cb96daed38f3cdd9be5ef897cc66c352ddcecea
JamesWClark <jwclark@rockhursths.edu>
2013-09-23 03:56:08 -0500

    started showing stats in console output

--- CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Run.java
+++ CS 303 Data Structures/Assignment 02/src/jwc62f/edu/umkc/assignment/two/Run.java
@@ -20,38 +20,50 @@ public class Run {
 			try {
 				switch(i) {
 					case 0: //selection sort
-						System.out.println("selection sort vs nearly sorted data:  " + Sort.Selection(numbers_nearlysorted)[0] + " milliseconds");
-						System.out.println("selection sort vs reverse sorted data: " + Sort.Selection(numbers_reverse)[0] + " milliseconds");
-						System.out.println("selection sort vs random sorted data:  " + Sort.Selection(numbers_random)[0] + " milliseconds");
+						Object[] stats = Sort.Selection(numbers_nearlysorted);
+						System.out.println("selection sort vs nearly sorted data:  " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Selection(numbers_reverse);
+						System.out.println("selection sort vs reverse sorted data: " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Selection(numbers_random);
+						System.out.println("selection sort vs random sorted data:  " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
 						break;
 					case 1: //bubble sort
-						System.out.println("bubble sort vs nearly sorted data:     " + Sort.Bubble(numbers_nearlysorted)[0] + " milliseconds");
-						System.out.println("bubble sort vs reverse sorted data:    " + Sort.Bubble(numbers_reverse)[0] + " milliseconds");
-						System.out.println("bubble sort vs random sorted data:     " + Sort.Bubble(numbers_random)[0] + " milliseconds");
+						stats = Sort.Bubble(numbers_nearlysorted);
+						System.out.println("bubble sort vs nearly sorted data:     " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Bubble(numbers_reverse);
+						System.out.println("bubble sort vs reverse sorted data:    " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Bubble(numbers_random);
+						System.out.println("bubble sort vs random sorted data:     " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
 						break;
 					case 2: //insertion sort
-						System.out.println("insertion sort vs nearly sorted data:  " + Sort.Insertion(numbers_nearlysorted)[0] + " milliseconds");
-						System.out.println("insertion sort vs reverse sorted data: " + Sort.Insertion(numbers_reverse)[0] + " milliseconds");
-						System.out.println("insertion sort vs random sorted data:  " + Sort.Insertion(numbers_random)[0] + " milliseconds");
+						stats = Sort.Insertion(numbers_nearlysorted);
+						System.out.println("insertion sort vs nearly sorted data:  " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Insertion(numbers_reverse);
+						System.out.println("insertion sort vs reverse sorted data: " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Insertion(numbers_random);
+						System.out.println("insertion sort vs random sorted data:  " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
 						break;
 					case 3: //shell
-						System.out.println("shell sort vs nearly sorted data:      " + Sort.Shell(numbers_nearlysorted)[0] + " milliseconds");
-						System.out.println("shell sort vs reverse sorted data:     " + Sort.Shell(numbers_reverse)[0] + " milliseconds");
-						System.out.println("shell sort vs random sorted data:      " + Sort.Shell(numbers_random)[0] + " milliseconds");
+						stats = Sort.Shell(numbers_nearlysorted);
+						System.out.println("shell sort vs nearly sorted data:      " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Shell(numbers_reverse);
+						System.out.println("shell sort vs reverse sorted data:     " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
+						stats = Sort.Shell(numbers_random);
+						System.out.println("shell sort vs random sorted data:      " + stats[0] + " milliseconds\t" + stats[1] + " iterations\t" + stats[2] + " swaps");
 						break;
 					case 4: //quick
 						long start = Calendar.getInstance().getTimeInMillis();
-						int[] stats = Sort.Quick(numbers_nearlysorted, 0, numbers_nearlysorted.length);
+						int[] quick_stats = Sort.Quick(numbers_nearlysorted, 0, numbers_nearlysorted.length);
 						long duration = Calendar.getInstance().getTimeInMillis() - start;
 						System.out.println("quick sort vs nearly sorted data:      " + duration + " milliseconds");
 						
 						start = Calendar.getInstance().getTimeInMillis();
-						stats = Sort.Quick(numbers_reverse, 0, numbers_reverse.length);
+						quick_stats = Sort.Quick(numbers_reverse, 0, numbers_reverse.length);
 						duration = Calendar.getInstance().getTimeInMillis() - start;
 						System.out.println("quick sort vs reverse sorted data:     " + duration + " milliseconds");
 						
 						start = Calendar.getInstance().getTimeInMillis();
-						stats = Sort.Quick(numbers_random, 0, numbers_random.length);
+						quick_stats = Sort.Quick(numbers_random, 0, numbers_random.length);
 						duration = Calendar.getInstance().getTimeInMillis() - start;
 						System.out.println("quick sort vs random sorted data:      " + duration + " milliseconds");
 						break;
