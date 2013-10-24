1708982c79a24b14b2c072d840924f3056356cb1
JamesWClark <jwclark@rockhursths.edu>
2013-10-24 00:39:26 -0500

    ds v2

--- j.web.umkc.edu slash jwc62f/js/DataStructures.js
+++ j.web.umkc.edu slash jwc62f/js/DataStructures.js
@@ -1,65 +1,25 @@
-/* LINKED LIST */
 
-//default constructor
-function LinkedList() {
-	return {
-		Count: 0,
-		First: null,
-		Last: null,
-		AddFirst: function(value) {
-			var node = LinkedListNode(value);
-			node.Next = this.First;
-			node.LinkedList = this;
-			this.First = node;
-			node.Next.Previous = node;
-			this.Count++;
-			if(this.Last === null) {
-				this.Last = node;
-			}
-		},
-		AddLast: function(value) {
-			var node = LinkedListNode(value);			
-			node.Previous = this.Last;
-			this.Last = node;
-			node.Previous.Next = node;
-			node.LinkedList = this;
-			this.Count++;
-			if(this.First === null) {
-				this.First = node;
-			}
-		},
-		ElementAt: function(index) {
-			var node = this.First;
-			for(var i = 1; i < index; i++) {
-				node = node.Next;
-			}
-			return node.Value;
-		},
-		Remove: function(node) {
-			if(node === this.First) {
-				this.First = node.Next;
-				//delete(node);
-			} else if(node === this.Last) {
-				this.Last = node.Previous;
-				//delete(node);
-			} else {
-				var prev = node.Previous;
-				var next = node.Next;
-				next.Prev = prev;
-				prev.Next = next;
-				//delete(node);
-			}
-			this.Count--;
-		}
-	}
-}
+function List(){ 
 
-/* LINKED LIST NODE */
-function LinkedListNode(value) {
-	return {
-		Value: value,
-		Next: null,
-		Previous: null,
-		LinkedList: null
-	}
+List.makeNode=function(){
+ return {data:null,next:null};
+};
+
+this.start=null;
+this.end=null;
+
+
+this.add=function (data){
+ if(this.start===null){
+   this.start=List.makeNode();
+   this.end=this.start;
+ }
+ else{
+  this.end.next=List.makeNode();
+  this.end=this.end.next;
+ };
+
+this.end.data=data;
+};
+
 }
