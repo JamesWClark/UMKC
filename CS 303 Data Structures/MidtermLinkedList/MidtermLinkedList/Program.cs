484919ae262e451b976f9bc67c2216018f742e85
JamesWClark <jwclark@rockhursths.edu>
2013-10-24 00:28:48 -0500

    messin' w/ javascript data structures

---
+++ CS 303 Data Structures/MidtermLinkedList/MidtermLinkedList/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MidtermLinkedList {
+    class Program {
+
+        private static LinkedList<int> set = new LinkedList<int>();
+
+        static void Main(string[] args) {
+            for (int i = 1; i < 6; i++) {
+                set.AddLast(i);
+            }
+            Filter(2, set.First);
+
+            for (int i = 0; i < set.Count; i++) {
+                Console.Write(set.ElementAt(i) + ", ");
+            }
+        }
+        static void Filter(int key, LinkedListNode<int> node) {
+            if (node.Value == key) {
+                LinkedListNode<int> next = node.Next;
+                set.Remove(next.Previous);
+                Filter(key, next);
+            } else if (node.Next != null) {
+                Filter(key, node.Next);
+            }
+        }
+    }
+}
