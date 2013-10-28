2480e88de1daf4c4ac635206606bf2e36912c61b
jwclark <jwclark@rockhursths.edu>
2013-10-28 16:06:25 -0500

    some homework added

--- CS 303 Data Structures/Assignment 06/FromMSDN/Program.cs
+++ CS 303 Data Structures/Assignment 06/FromMSDN/Program.cs
@@ -1,11 +1,17 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using skmDataStructures;
 
 namespace FromMSDN {
     class Program {
         static void Main(string[] args) {
+
+        }
+        public void InorderTraversal(Node current) {
+            if (current != null) {
+                InorderTraversal(current.Left);
+                Console.WriteLine(current.Value + "\n");
+                InorderTraversal(current.Right);
+            }
         }
     }
 }
